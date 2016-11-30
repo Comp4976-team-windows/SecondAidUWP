3538e52d7eb0a73a4a93f7531f25580d2647904c
chaoswlz <wulize1994@gmail.com>
2016-11-30 15:38:52 -0800

    update

--- SecondAidUWP/SecondAidUWP/MainPage.xaml.cs
+++ SecondAidUWP/SecondAidUWP/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SecondAidUWP.Model;
+using System;
+using System.Net;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +14,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using Windows.Data.Json;
+using System.Text;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +31,44 @@ namespace SecondAidUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var name = UserNameTextBox.Text;
+            var password = PasswordTextBox.Password;
+            var url = Config.connectTokenUrl;
+            string myParameters = "userName=" + name + "&password=" + password + "&grant_type=password";
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage()
+                    {
+                        RequestUri = new Uri(url),
+                        Method = HttpMethod.Post,
+                    };
+                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+                    
+                    //grab json from server
+                    var json = await client.SendAsync(request, myParameters);
+                    UserNameTextBox.Text = json.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex);
+            }
+
+
+            
+            
+
         }
     }
 }
---
+++ SecondAidUWP/SecondAidUWP/Model/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondAidUWP.Model
+{
+    class Config
+    {
+        public static string connectTokenUrl = "http://2aid.azurewebsites.net/connect/token";
+    }
+}
