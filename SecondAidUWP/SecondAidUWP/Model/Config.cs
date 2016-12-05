2d9e1efb8343365dd19f2636cb60c242b342b2d9
chaoswlz <wulize1994@gmail.com>
2016-12-04 16:39:53 -0800

    add url and schedule model

--- SecondAidUWP/SecondAidUWP/Model/Config.cs
+++ SecondAidUWP/SecondAidUWP/Model/Config.cs
@@ -11,7 +11,12 @@ namespace SecondAidUWP.Model
         //Urls
         public static string connectTokenUrl = "http://2aid.azurewebsites.net/connect/token";
         public static string procedureApiUrl = "http://2aid.azurewebsites.net/api/procedures";
-
+        public static string subProcedureApiUrl = "http://2aid.azurewebsites.net/api/subprocedure";
+        public static string PreInstructionApiUrl = "http://2aid.azurewebsites.net/api/preinstructione";
+        public static string questionnaireApiUrl = "http://2aid.azurewebsites.net/api/questionnaires";
+        public static string scheduleApiUrl = "http://2aid.azurewebsites.net/api/schedules";
+        public static string medicationInstruction = "http://2aid.azurewebsites.net/api/medicationinstructions";
+        public static string medication = "http://2aid.azurewebsites.net/api/medications";
         //Form variables
         public static MainPage MainPage;
         public static ProcedureListPage ProcedureListPage;
